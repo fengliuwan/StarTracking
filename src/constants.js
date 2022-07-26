f92be250e304201912369e3dd0fa39b3a8115843
fengliuwan <fengwuyan23@gmail.com>
2022-07-26 00:19:53 -0400

    project upload

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const SAT_API_KEY = "42422Z-33P87G-RLVQNN-4WF9";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://ec2-3-134-91-227.us-east-2.compute.amazonaws.com";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
